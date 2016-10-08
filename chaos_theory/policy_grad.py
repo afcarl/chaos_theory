977068aa94c9faf78d19ddbd74ea23098b5ad4f7
Justin Fu <justinjfu@gmail.com>
2016-10-07 17:44:13 -0700

    Baselines

--- chaos_theory/policy_grad.py
+++ chaos_theory/policy_grad.py
@@ -1,7 +1,7 @@
 import numpy as np
 from utils import discount_rew
 
-def reinforce_grad(pol, trajlist, disc=0.9):
+def reinforce_grad(pol, trajlist, disc=0.9, baseline=None):
     """
     Compute policy gradient using REINFORCE
 
@@ -13,14 +13,18 @@ def reinforce_grad(pol, trajlist, disc=0.9):
     for traj in trajlist:
         T = traj.T
         for t in range(T):
+            obs_t = traj.obs[t]
+            act_t = traj.act[t]
+
             r_t = np.sum(discount_rew(traj.rew[t:], gamma=disc))
             advantage_t = r_t  # No baseline
+            if baseline is not None:
+                advantage_t = r_t - baseline.eval(obs_t)
 
-            grad_prob = pol.grad_act(traj.act[t], traj.obs[t])
-            act_prob = pol.prob_act(traj.act[t], traj.obs[t])
+            grad_prob = pol.grad_act(act_t, obs_t)
+            act_prob = pol.prob_act(act_t, obs_t)
 
             # grad = d logprob/dtheta  * advantage
             grad += advantage_t * (1./act_prob)*grad_prob
-            #grad += advantage_t * grad_prob
     grad = grad/len(trajlist)
     return grad
